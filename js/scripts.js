871dc259e13c3a579d386561cec2acebf4dba7c7
Andres Garcia and zachary baum <andres@>
2016-08-08 15:24:43 -0400

    upload final javascript code

--- js/scripts.js
+++ js/scripts.js
@@ -21,3 +21,18 @@ var cipher3 = function(sentence, cipher2) {
 }
 
 alert(cipher3(sentence, cipher2));
+
+var cipher4 = function(sentence){
+var index =  sentence.length / 2;
+var indexr = Math.round(index);
+var char = sentence.charAt(indexr);
+return char;
+}
+alert(cipher4(sentence) + cipher3(sentence, cipher2));
+
+var final = cipher4(sentence) + cipher3(sentence, cipher2);
+
+function reverse(final){
+return final.split('').reverse().join('');
+}
+alert(reverse(final));
